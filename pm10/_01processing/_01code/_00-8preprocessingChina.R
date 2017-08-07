fea72f91a74b5bc30c00ef1a0b09e9f07137e706
KEIBigdataResearch <jinhyeongkim@kei.re.kr>
2017-08-07 16:55:55 +0900

    preprocessing China data

--- pm10/_01processing/_01code/_00-8preprocessingChina.R
+++ pm10/_01processing/_01code/_00-8preprocessingChina.R
@@ -3,6 +3,16 @@
 #remove variable
 rm(list=ls())
 
+#define function
+narmMean <- function(values){
+  if(length(values[values!=NA])>=540){
+    mean(values, na.rm=TRUE)
+  } else {
+    NA
+  }
+}
+
+#load data
 b_08 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Beijing_2008.csv")
 b_09 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Beijing_2009.csv")
 b_10 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Beijing_2010.csv")
@@ -13,17 +23,38 @@ b_14 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preproces
 b_15 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Beijing_2015.csv")
 b_16 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Beijing_2016.csv")
 
+#merge
 bData = rbind(b_08, b_09, b_10, b_11, b_12, b_13, b_14, b_15, b_16)
-bData[bData$Value == -999, "value"] <- NA
 
+#missing data
+bData[bData$Value == -999, "Value"] <- NA
+
+#aggregate
+bData <- bData[,c("Year","Month","Value")]
 bData.aggr <- aggregate(x=bData[c("Value")],
-                             by=aggregate[c("YEAR","SIDO","SIGUNGU","SIGUNGU2","SIG_CD","LARGECATE")],
-                             FUN=sum,
-                             na.rm=TRUE)
+                             by=bData[c("Year","Month")],
+                             FUN=narmMean) #,
+                             #na.rm=TRUE)
 
 
+##mean of Shanghai
+#load data
+s_11 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Shanghai_2011.csv")
+s_12 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Shanghai_2012.csv")
+s_13 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Shanghai_2013.csv")
+s_14 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Shanghai_2014.csv")
+s_15 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Shanghai_2015.csv")
+s_16 = read.csv("D:/my-backup/project/pm10/_01processing/_00input/_00-8preprocessingChina/Shanghai_2016.csv")
 
+#merge
+sData = rbind(s_11, s_12, s_13, s_14, s_15, s_16)
 
+#missing data
+sData[sData$Value == -999, "Value"] <- NA
 
+#aggregate
+sData <- sData[,c("Year","Month","Value")]
+sData.aggr <- aggregate(x=sData[c("Value")],
+                        by=sData[c("Year","Month")],
+                        FUN=narmMean)
 
-SO2_mean <- aggregate(dataMonth$SO2~dataMonth$month+dataMonth$area, dataMonth, narmMean)
--- pm10/_01processing/_01code/_04merge.R
+++ pm10/_01processing/_01code/_04merge.R
@@ -5,9 +5,9 @@ rm(list=ls())
 
 ##weather data 
 weatherMonth <- read.csv("monthResult_weather.csv", header=T, sep=",")
-pollutantMonth <- read.csv("pm10_sig_matching_75_mean_agg.csv", header=T, sep=",")
+pollutantMonth <- read.csv("D:/my-backup/project/pm10/_01processing/_02output/_03meanOfAirPollutionOfSigungu/pm10_sig_matching_75_mean_agg.csv", header=T, sep=",")
 
-yearMonthWeather <- paste(weatherMonth$year, weatherMonth$month, weatherMonth$ì§€ì )
+yearMonthWeather <- paste(weatherMonth$year, weatherMonth$month, weatherMonth$ì§€? )
 yearMonthWeatherInPollutant <- paste(pollutantMonth$year, pollutantMonth$month, pollutantMonth$weather_cd)
 
 weatherMonth <- cbind(weatherMonth, day=yearMonthWeather)
